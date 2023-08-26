a475e57f54c859cf8a343fcdb91bc863414ec628
Gyrotank <Dmytro_Glomozda@epam.com>
2023-08-26 18:56:04 +0300

    - added plugin section to CucumberOptions annotation to enable generation of HTML and JSON Cucumber reports

--- src/main/java/com/iosnasu/hrmanagementtaf/CucumberTest.java
+++ src/main/java/com/iosnasu/hrmanagementtaf/CucumberTest.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"classpath:features"},
-        glue = {"com.iosnasu.hrmanagementtaf", "com.iosnasu.hrmanagementtaf.stepdefs"}
+        glue = {"com.iosnasu.hrmanagementtaf", "com.iosnasu.hrmanagementtaf.stepdefs"},
+        plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json"}
 )
 public class CucumberTest {
 }
