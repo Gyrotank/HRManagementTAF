ff8c1e6fc1300ac88b87a15c1d2ef4810e055b49
Dmytro Glomozda <Dmytro_Glomozda@epam.com>
2023-10-20 13:11:20 +0300

    - added ExtentReport generation

--- src/main/java/com/iosnasu/hrmanagementtaf/CucumberTest.java
+++ src/main/java/com/iosnasu/hrmanagementtaf/CucumberTest.java
@@ -8,7 +8,12 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = {"classpath:features"},
         glue = {"com.iosnasu.hrmanagementtaf", "com.iosnasu.hrmanagementtaf.stepdefs"},
-        plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber-report.json"}
+        plugin = {"pretty",
+                "html:target/cucumber-report.html",
+                "json:target/cucumber-report.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        },
+        monochrome = true
 )
 public class CucumberTest {
 }
